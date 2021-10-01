fb03b2c00067b300cdd2bacefc7654c60f75a232
asasn <as_asn@qq.com>
2021-10-02 02:23:40 +0800

    基本界面布局

---
+++ uc-BookTree.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 脸滚键盘
+{
+    /// <summary>
+    /// uc_BookTree.xaml 的交互逻辑
+    /// </summary>
+    public partial class uc_BookTree : UserControl
+    {
+        public uc_BookTree()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public string UcTitle
+        {
+            get { return (string )GetValue(UcTitleProperty); }
+            set { SetValue(UcTitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UcTitle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UcTitleProperty =
+            DependencyProperty.Register("UcTitle", typeof(string ), typeof(uc_BookTree), new PropertyMetadata(null));
+
+
+    }
+}
