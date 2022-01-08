e1e0cfc2f34372a9b35208d17d1844f7f1d9f745
asasn <as_asn@qq.com>
2022-01-08 15:23:16 +0800

    准备利用自定义控件UcRecords取代一些角色卡设计模块

---
+++ 自定义控件/UcRecords.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 脸滚键盘.自定义控件
+{
+    /// <summary>
+    /// UcRecords.xaml 的交互逻辑
+    /// </summary>
+    public partial class UcRecords : UserControl
+    {
+        public UcRecords()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            WpMain.Children.Add(new UcTipBox());
+        }
+    }
+}
