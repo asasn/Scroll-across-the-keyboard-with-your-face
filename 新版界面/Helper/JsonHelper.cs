85560351aa4ae96e449d6974f666bfa342c93fa5
asasn <as_asn@qq.com>
2022-05-27 02:01:15 +0800

    重新改回了Json的转换引用包

--- 新版界面/Helper/JsonHelper.cs
+++ 新版界面/Helper/JsonHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Text.Json;
+using System.Web.Script.Serialization;
 
 namespace RootNS.Helper
 {
@@ -27,5 +29,34 @@ namespace RootNS.Helper
             return JsonSerializer.Deserialize<T>(jsonString);
         }
 
+
+        ///// <summary>
+        ///// 转换成Json
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //public static string ObjectToJson<T>(T obj)
+        //{
+        //    JavaScriptSerializer jserializer = new JavaScriptSerializer();
+        //    return jserializer.Serialize(obj);
+        //}
+        ///// <summary>
+        ///// 从Json中读取
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //public static T JsonToObject<T>(string strJson)
+        //{
+        //    JavaScriptSerializer jserializer = new JavaScriptSerializer();
+        //    try
+        //    {
+        //        return jserializer.Deserialize<T>(strJson);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(String.Format("解析错误 - {0}！", ex));
+        //        return default;
+        //    }
+        //}
     }
 }
