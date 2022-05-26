dedd05d31dedf94cc90aa4d5c3653f99a8b79038
asasn <as_asn@qq.com>
2022-05-27 02:10:23 +0800

    重新设置Json的引用类为JavaScriptSerializer

--- 新版界面/Helper/JsonHelper.cs
+++ 新版界面/Helper/JsonHelper.cs
@@ -1,62 +1,62 @@
 ﻿using System;
-using System.Text.Json;
+//using System.Text.Json;
 using System.Web.Script.Serialization;
 
 namespace RootNS.Helper
 {
     public class JsonHelper
     {
-        /// <summary>
-        /// 序列化为JsonString
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public static string ObjectToJson<T>(T obj)
-        {
-            return JsonSerializer.Serialize(obj);
-        }
-        /// <summary>
-        /// 反序列化JsonString为对象
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public static T JsonToObject<T>(string jsonString)
-        {
-            if (string.IsNullOrWhiteSpace(jsonString))
-            {
-                return default;
-            }
-            return JsonSerializer.Deserialize<T>(jsonString);
-        }
-
-
         ///// <summary>
-        ///// 转换成Json
+        ///// 序列化为JsonString
         ///// </summary>
         ///// <param name="sender"></param>
         ///// <param name="e"></param>
         //public static string ObjectToJson<T>(T obj)
         //{
-        //    JavaScriptSerializer jserializer = new JavaScriptSerializer();
-        //    return jserializer.Serialize(obj);
+        //    return JsonSerializer.Serialize(obj);
         //}
         ///// <summary>
-        ///// 从Json中读取
+        ///// 反序列化JsonString为对象
         ///// </summary>
         ///// <param name="sender"></param>
         ///// <param name="e"></param>
-        //public static T JsonToObject<T>(string strJson)
+        //public static T JsonToObject<T>(string jsonString)
         //{
-        //    JavaScriptSerializer jserializer = new JavaScriptSerializer();
-        //    try
+        //    if (string.IsNullOrWhiteSpace(jsonString))
         //    {
-        //        return jserializer.Deserialize<T>(strJson);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(String.Format("解析错误 - {0}！", ex));
         //        return default;
         //    }
+        //    return JsonSerializer.Deserialize<T>(jsonString);
         //}
+
+
+        /// <summary>
+        /// 转换成Json
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static string ObjectToJson<T>(T obj)
+        {
+            JavaScriptSerializer jserializer = new JavaScriptSerializer();
+            return jserializer.Serialize(obj);
+        }
+        /// <summary>
+        /// 从Json中读取
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static T JsonToObject<T>(string strJson)
+        {
+            JavaScriptSerializer jserializer = new JavaScriptSerializer();
+            try
+            {
+                return jserializer.Deserialize<T>(strJson);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(String.Format("解析错误 - {0}！", ex));
+                return default;
+            }
+        }
     }
 }
